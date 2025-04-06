fb9c08a8b14de3ee251025aed131c96bba49a468
SHYLESH S <shylesh1640@gmail.com>
2025-04-06 16:46:45 +0530

    Update Linear Algebra.py
    13th day

--- Linear Algebra.py
+++ Linear Algebra.py
@@ -409,3 +409,25 @@ def svd_2x2_singular_values(A):
     return U, sigma, V
 
 
+'''Linear Regression Using Normal Equation
+
+Write a Python function that performs linear regression using the normal equation. The function should take a matrix X (features) and a vector y (target) as input, and return the coefficients of the linear regression model. Round your answer to four decimal places, -0.0 is a valid result for rounding a very small number.
+
+Example:
+Input:
+X = [[1, 1], [1, 2], [1, 3]], y = [1, 2, 3]
+Output:
+[0.0, 1.0]
+Reasoning:
+The linear model is y = 0.0 + 1.0*x, perfectly fitting the input data.'''
+
+
+import numpy as np
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+	X = np.array(X)
+    y = np.array(y)
+    XtX = X.T @ X
+    XtX_inv = np.linalg.inv(XtX)
+    Xt_y = X.T @ y
+    theta = XtX_inv @ Xt_y
+	return theta
