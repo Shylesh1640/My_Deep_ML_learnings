e2d6caf52357712a045d331f7fda4a43c67e31b8
SHYLESH S <shylesh1640@gmail.com>
2025-04-18 17:08:58 +0530

    Update Linear Algebra.py
    24th day

--- Linear Algebra.py
+++ Linear Algebra.py
@@ -744,6 +744,42 @@ def shuffle_data(X, y, seed=None):
     return X_shuffled, y_shuffled
 
 
+'''Batch Iterator for Dataset
 
+Implement a batch iterable function that samples in a numpy array X and an optional numpy array y. The function should yield batches of a specified size. If y is provided, the function should yield batches of (X, y) pairs; otherwise, it should yield batches of X only.
+
+Example:
+Input:
+X = np.array([[1, 2], 
+                  [3, 4], 
+                  [5, 6], 
+                  [7, 8], 
+                  [9, 10]])
+    y = np.array([1, 2, 3, 4, 5])
+    batch_size = 2
+    batch_iterator(X, y, batch_size)
+Output:
+[[[[1, 2], [3, 4]], [1, 2]],
+     [[[5, 6], [7, 8]], [3, 4]],
+     [[[9, 10]], [5]]]
+Reasoning:
+The dataset X contains 5 samples, and we are using a batch size of 2. Therefore, the function will divide the dataset into 3 batches. The first two batches will contain 2 samples each, and the last batch will contain the remaining sample. The corresponding values from y are also included in each batch'''
+
+
+import numpy as np
+
+def batch_iterator(X, y=None, batch_size=64):
+    n_samples = X.shape[0]
+    batches = []
+
+    for i in range(0, n_samples, batch_size):
+        X_batch = X[i:i + batch_size].tolist()
+        if y is not None:
+            y_batch = y[i:i + batch_size].tolist()
+            batches.append([X_batch, y_batch])
+        else:
+            batches.append(X_batch)
+    
+    return batches
 
 
